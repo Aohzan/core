320bcb441cc9156c9743559e6973356cca2cfdf0
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2023-11-14 15:58:13 +0000

    fix option

--- homeassistant/components/ipx800v5/light.py
+++ homeassistant/components/ipx800v5/light.py
@@ -4,14 +4,14 @@ import logging
 from typing import Any
 
 from pypx800v5 import (
-    EXT_X8R,
     EXT_X010V,
+    EXT_X8R,
     EXT_XDIMMER,
     EXT_XPWM,
     IPX,
     IPX800,
-    X8R,
     X010V,
+    X8R,
     XPWM,
     IPX800Relay,
     XDimmer,
@@ -54,19 +54,9 @@ def scalefrom100to255(value):
     return max(0, min(255, round((value * 255.0) / 100.0)))
 
 
-def scalefrom10to255(value):
-    """Scale from X-010V value to Home-Assistant value."""
-    return max(0, min(255, round((value * 10 * 255.0) / 100.0)))
-
-
 def scaleto100(value):
     """Scale to IPX800 value."""
     return max(0, min(100, round((value * 100.0) / 255.0)))
-
-
-def scaleto10(value):
-    """Scale to X-010V value."""
-    return max(0, min(10, round((value * 100.0) / 255.0 / 10)))
 
 
 async def async_setup_entry(
@@ -567,14 +557,14 @@ class X010VLight(IpxEntity, LightEntity):
     @property
     def brightness(self) -> int:
         """Return the brightness of the output."""
-        return scalefrom10to255(
+        return scalefrom100to255(
             self.coordinator.data[self.control.ana_level_id]["value"]
         )
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on the output."""
         if ATTR_BRIGHTNESS in kwargs:
-            await self.control.set_level(scaleto10(kwargs[ATTR_BRIGHTNESS]))
+            await self.control.set_level(scaleto100(kwargs[ATTR_BRIGHTNESS]))
         else:
             await self.control.on()
         await self.coordinator.async_request_refresh()
